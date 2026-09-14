e190fb37ad9de79bf5c7003e4edb1c4280effefd
cmj <cmj@cmj.tw>
2026-09-14 15:22:15 +0800

    fix(ui): hide the timeline scrollbar
        The stream still scrolls. The bar does not draw.

--- Sources/FediqoUI/Shell/TimelinePane.swift
+++ Sources/FediqoUI/Shell/TimelinePane.swift
@@ -38,6 +38,7 @@ struct TimelinePane: View {
                         }
                     }
                 }
+                .scrollIndicators(.hidden)
             }
         }
         .overlay(alignment: .bottom) {
